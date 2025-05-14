5b3991e7c91f2e8a24d414e3250f7bd0d38d8870
huza <huzaifahasif003@gmail.com>
2025-05-14 23:16:49 +0700

    refactor gallery to dynamically load images from JSON data

--- xirpl1/src/Components/Gallery.js
+++ xirpl1/src/Components/Gallery.js
@@ -1,42 +1,43 @@
 import React, { Component } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
+import data from "./data.json"; // Import data JSON
 
 let id = 0;
+
 class Portfolio extends Component {
   render() {
-    if (!this.props.data) return null;
+    if (!data) return null; // Kalau datanya nggak ada, nggak tampil
 
-    const projects = this.props.data.projects.map(function (projects) {
-      let projectImage = "assets/img/logooLove.png";
+    const projects = data.projects.map(function (project) {
+      let projectImage = project.image || "assets/img/huza.png";  // Default gambar jika nggak ada
 
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <Zmage alt={project.title} src={projectImage} />
+            <div style={{ textAlign: "center" }}>{project.title}</div>
           </div>
         </div>
       );
     });
 
     return (
+      <section id="gallery">
       <section id="portfolio">
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
               <h1>Our Random Pictures</h1>
 
-              <div
-                id="portfolio-wrapper"
-                className="bgrid-quarters s-bgrid-thirds cf"
-              >
+              <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
               </div>
             </div>
           </div>
         </Fade>
       </section>
+    </section>
     );
   }
 }
